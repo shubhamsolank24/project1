6d62b75e20f436ad140f3cbc0422ac0d60a4c56f
shubhamsolank24 <67789640+shubhamsolank24@users.noreply.github.com>
2020-07-05 22:46:08 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -3,6 +3,8 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
+from collections import defaultdict
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -11,6 +13,28 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+    calls_dict = defaultdict(int)
+
+for caller, callee, timestamp, duration in calls:
+    calls_dict[caller] += int(duration)
+    calls_dict[callee] += int(duration)
+
+max_time = 0
+phone_number = ""       
+
+for key,value in calls_dict.items():
+    if value > max_time:
+        phone_number = key
+        max_time = value
+
+print(f"{phone_number} spent the longest time, {max_time} seconds, on the phone during September 2016.")
+
+    
+    
+    
+    
+    
+    
 """
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
@@ -19,4 +43,3 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
-
